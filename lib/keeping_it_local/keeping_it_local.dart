c0db8cda68cbff54cda8e36f519dee359f889e1b
Tobias Busch <tobi@minimalme.de>
2021-02-21 20:32:20 +0100

    Add sharedpref sample

--- lib/keeping_it_local/keeping_it_local.dart
+++ lib/keeping_it_local/keeping_it_local.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class KeepingItLocal extends StatefulWidget {
   @override
@@ -6,8 +8,56 @@ class KeepingItLocal extends StatefulWidget {
 }
 
 class _KeepingItLocalState extends State<KeepingItLocal> {
+  int _counter = 0;
+  final String _counterKey = "counter";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadCounter();
+  }
+
+  _loadCounter() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (preferences.getInt(_counterKey) ?? 0);
+    });
+  }
+
+  _incrementCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (prefs.getInt('counter') ?? 0) + 1;
+      prefs.setInt('counter', _counter);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Keeping it local"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
